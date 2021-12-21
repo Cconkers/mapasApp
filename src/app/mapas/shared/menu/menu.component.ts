c7092847f0a2016b0a610a09b6e2377f0011b296
Cconkers <rubenpv011992@gmail.com>
2021-12-21 15:41:03 +0100

    Uploading content

---
+++ src/app/mapas/shared/menu/menu.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  ruta: string;
+  nombre: string;
+}
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: [],
+})
+export class MenuComponent {
+  menuItem: MenuItem[] = [
+    {
+      ruta: '/mapas/fullscreen',
+      nombre: 'FullScreen',
+    },
+    {
+      ruta: '/mapas/zoom-range',
+      nombre: 'Zoom Range',
+    },
+    {
+      ruta: '/mapas/propiedades',
+      nombre: 'Propiedades',
+    },
+    {
+      ruta: '/mapas/marcadores',
+      nombre: 'Marcadores',
+    },
+  ];
+}
